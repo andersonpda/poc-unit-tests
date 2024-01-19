e877657e9e12b1f87777a2c597f45f79d08d782d
Anderson Sapucaia <anderson.dev@outlook.com>
2024-01-19 14:57:56 -0300

    commit inicial

---
+++ src/WebApplication1/Models/AppDbContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>()
+            .HasKey(p => p.Id);
+
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Description)
+            .HasMaxLength(500);
+    }
+}
